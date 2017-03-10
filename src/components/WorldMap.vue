954f252356377bf8a03667e73b43dc41268a22ea
Taras Novak <taras.novak@randomfractals.com>
2017-03-10 17:48:36 -0600

    map container div/css setup/cleanup

--- src/components/WorldMap.vue
+++ src/components/WorldMap.vue
@@ -1,6 +1,6 @@
 <template>
-  <div id="map" class="map">
-    <h1>{{ msg }}</h1>
+  <div id="map" class="map container">
+    <span>{{ msg }}</span>
   </div>
 </template>
 
@@ -17,29 +17,8 @@ export default {
 
 <style scoped>
 #map {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+  margin: 0px;
+  height: 100%;
+  width: 100%;  
 }
 </style>
