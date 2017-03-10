4a1034e0629bd3f2f67f5b7b4f5321bd36775b1c
Taras Novak <taras.novak@randomfractals.com>
2017-03-10 17:49:05 -0600

    added basic app styles and moved toolbar to ToolBar.vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,56 @@
 <template>
   <div id="app">
-    <md-toolbar>
-      <h1 class="md-title">world migrations</h1>
-    </md-toolbar>
-    <worldMap></worldMap>
+    <toolBar></toolBar>
+    <div class="content">
+      <worldMap></worldMap>
+    </div>
   </div>
 </template>
 
 <script>
+import ToolBar from './components/ToolBar.vue'
 import WorldMap from './components/WorldMap.vue'
 
 export default {
   name: 'app',
   components: {
+    ToolBar,
     WorldMap
   }
 }
 </script>
+
+<style>
+  html,
+  body, 
+  content,
+  #app {
+    height: 100%;
+    overflow: hidden;
+  }
+
+  body, #app {
+    margin: 0px;
+  }
+
+  .md-toolbar {
+    min-height: 32px;
+    border-bottom: 1px solid rgba(#000, .12);
+  }
+
+ .container {
+    min-height: 100%;
+    display: flex;
+    flex-flow: column nowrap;
+    flex: 1;
+  }  
+
+  .content {
+    padding: 16px;
+    flex: 1;
+    overflow: auto;
+    background-color: #fff;
+  }
+  
+</style>
+
---
+++ src/components/ToolBar.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="md-toolbar-container">
+  <md-toolbar id="toolbar" class="md-large">
+    <h3 class="md-title" style="flex: 1">{{ title }}</h3>
+  </md-toolbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'toolBar',
+  data () {
+    return {
+      title: 'World Migrations'
+    }
+  }
+}
+</script>
+
+<style>
+.md-title {
+  font-weight: normal;
+  margin: 0px;
+  padding: 10px;
+}
+</style>
