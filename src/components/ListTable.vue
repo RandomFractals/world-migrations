15d320bc13657b0fc0d55cd6ab8db6e4e3c4141e
Taras Novak <taras.novak@randomfractals.com>
2017-03-11 09:08:40 -0600

    added list table card stub

---
+++ src/components/ListTable.vue
@@ -0,0 +1,41 @@
+<template>
+  <md-card class="card list-card">
+    <md-card-area md-inset>
+      <md-card-header class="card-header">
+        <div class="md-title">{{ title }}</div>
+        <div class="md-subhead">{{ subtitle }}</div>      
+      </md-card-header>
+      <md-card-content>
+        <div id="data-list" class="list-container">
+          [list data]
+        </div>
+      </md-card-content>
+    </md-card-area>
+  </md-card>
+</template>
+
+<script>
+export default {
+  name: 'listTable',
+  data () {
+    return {
+      title: '[list title]',
+      subtitle: '[subtitle]'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.list-card {
+  width: 240px;
+}
+
+.list-container {
+  margin: 0px;
+  height: 100%;
+  width: 100%;
+  overflow-y: scroll;  
+}
+</style>
